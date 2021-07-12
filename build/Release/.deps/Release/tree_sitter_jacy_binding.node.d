1fdb4db88363fa8534b145d0fda187ce841b8944
hazer-hazer <gordeev.john@gmail.com>
2021-07-12 19:21:08 +0300

    Update

--- build/Release/.deps/Release/tree_sitter_jacy_binding.node.d
+++ build/Release/.deps/Release/tree_sitter_jacy_binding.node.d
@@ -1,1 +1,1 @@
-cmd_Release/tree_sitter_jacy_binding.node := rm -rf "Release/tree_sitter_jacy_binding.node" && cp -af "Release/obj.target/tree_sitter_jacy_binding.node" "Release/tree_sitter_jacy_binding.node"
+cmd_Release/tree_sitter_jacy_binding.node := ln -f "Release/obj.target/tree_sitter_jacy_binding.node" "Release/tree_sitter_jacy_binding.node" 2>/dev/null || (rm -rf "Release/tree_sitter_jacy_binding.node" && cp -af "Release/obj.target/tree_sitter_jacy_binding.node" "Release/tree_sitter_jacy_binding.node")
